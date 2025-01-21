fabb697c21dd5afb78190185130b6c4f87275912
imadiss <imadissami2004@gmail.com>
2025-01-21 18:25:13 +0100

    deployment

--- analysis/my_model.py
+++ analysis/my_model.py
@@ -1,14 +1,20 @@
 import pandas as pd
 import joblib
 
-v=joblib.load("vectorizer.pkl")
-model=joblib.load("sentiment_analysis_model.pkl")
+v=joblib.load("vectorizer2.pkl")
+model=joblib.load("sentiment_analysis_model2.pkl")
+
+
+
+
 
 def prediction(text):
     t=v.transform([text])
     res=model.predict(t)
     if res[0]==1:
-        return "postive"
-    else: return "negative"
+        return "Postive"
+    elif res[0]==0:
+        return "Neutral"
+    else: return "Negative"
 
 

63706785ad9814e4147939c6b201817243a53953
imadiss <imadissami2004@gmail.com>
2025-01-21 12:34:56 +0100

    deployment

--- analysis/my_model.py
+++ analysis/my_model.py
@@ -14,23 +14,12 @@ df_test=pd.read_csv(dataset_path_test)
 df=df.dropna()
 df_test['text']=df_test['text'].str.lower()
 
-from sklearn.naive_bayes import MultinomialNB
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score, classification_report
-from sklearn.feature_extraction.text import CountVectorizer,TfidfVectorizer
-from sklearn.ensemble import RandomForestClassifier, VotingClassifier
-from sklearn import svm
+
+from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.linear_model import LogisticRegression
-from sklearn.tree import DecisionTreeClassifier
 
 
-model1=svm.SVC(kernel='linear')
-model2=RandomForestClassifier(n_estimators=100)
-model3=MultinomialNB()
-model4=LogisticRegression(max_iter=10)
-model5=DecisionTreeClassifier()
-
-model=model4
+model=LogisticRegression(max_iter=10)
 
 
 
@@ -38,14 +27,12 @@ x=df['text']
 v=CountVectorizer()
 x=v.fit_transform(x)
 y=df['label'].to_numpy()
-#x_train,x_test,y_train,y_test=train_test_split(x_num,y,random_state=1,test_size=0.2)
+
 model.fit(x,y)
 
 x_test=v.transform(df_test['text'])
 y_test=df_test['label'].to_numpy()
 y_predict=model.predict(x_test)
-#print('Accuracy: ',str(accuracy_score(y_test,y_predict)*100)+'%')
-#print('Report:\n',classification_report(y_test, y_predict))
 
 def prediction(text):
     t=v.transform([text])

06c7a5b1a62e9865a8592d5614b4dfba30e7ef6f
imadiss <imadissami2004@gmail.com>
2025-01-25 19:49:24 +0100

    deployment

--- analysis/views.py
+++ analysis/views.py
@@ -8,9 +8,9 @@ def index(request):
         text = request.POST.get('text')
         if text:
             res = prediction(text)
-            client_ip = request.META.get('HTTP_X_FORWARDED_FOR', request.META.get('REMOTE_ADDR'))
-            with connection.cursor() as cursor:
-                cursor.execute("INSERT INTO DATA (INPUT, OUTPUT, IP_ADDRESS) VALUES (%s, %s, %s)", [text, res, client_ip])
+            # client_ip = request.META.get('HTTP_X_FORWARDED_FOR', request.META.get('REMOTE_ADDR'))
+            # with connection.cursor() as cursor:
+            #     cursor.execute("INSERT INTO DATA (INPUT, OUTPUT, IP_ADDRESS) VALUES (%s, %s, %s)", [text, res, client_ip])
         else:
             res = "Please enter some text for analysis."
     return render(request, 'index.html', {'result': res})
--- sentiment_analysis/settings.py
+++ sentiment_analysis/settings.py
@@ -76,9 +76,9 @@ WSGI_APPLICATION = 'sentiment_analysis.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-DATABASES = {
-    'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))
-}
+# DATABASES = {
+#     'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))
+# }
 
 
 # Password validation

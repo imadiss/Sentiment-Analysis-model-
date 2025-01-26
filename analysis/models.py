c197729cbe5b06f904298475408db36aff048c6b
imadiss <imadissami2004@gmail.com>
2025-01-26 18:12:22 +0100

    deployment

--- analysis/models.py
+++ analysis/models.py
@@ -1,5 +1,3 @@
 from django.db import models
 
-class Data(models.Model):
-    input_text = models.TextField()
-    output_text = models.CharField(max_length=10)
+
--- analysis/views.py
+++ analysis/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from .my_model import prediction
 from django.db import connection
-
+import datetime as dt
 def index(request):
     res = None
     if request.method == "POST":
@@ -9,8 +9,9 @@ def index(request):
         if text:
             res = prediction(text)
             client_ip = request.META.get('HTTP_X_FORWARDED_FOR', request.META.get('REMOTE_ADDR'))
+            date_time=dt.datetime.now()
             with connection.cursor() as cursor:
-                cursor.execute("INSERT INTO DATA (INPUT, OUTPUT, IP_ADDRESS) VALUES (%s, %s, %s)", [text, res, client_ip])
+                cursor.execute("INSERT INTO DATA (INPUT, OUTPUT, IP_ADDRESS, DATE_TIME) VALUES (%s, %s, %s, %s)", [text, res, client_ip,date_time])
         else:
             res = "Please enter some text for analysis."
     return render(request, 'index.html', {'result': res})

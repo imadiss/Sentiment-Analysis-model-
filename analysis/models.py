397073b86c499c3dd150e89779c5ccdc43e9f3b5
imadiss <imadissami2004@gmail.com>
2025-01-26 22:12:47 +0100

    deployment

--- analysis/models.py
+++ analysis/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-
+class Data(models.Model):
+    id = models.AutoField(primary_key=True)
+    input = models.TextField()
+    output = models.CharField(max_length=10)
+    ip_address = models.CharField(max_length=50)
+    date_time = models.DateTimeField()
